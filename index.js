12a67d2305ccc7882cf98450f98b93d48623fb39
Jeremy Schulz <Jeremy.Schulz@noaa.gov>
2022-08-16 12:17:28 -0600

    Allow for script to finish if files aren't available.

--- index.js
+++ index.js
@@ -13,27 +13,31 @@
 const fs = require('fs');
 const open = require("open");
 
-let pwd = process.env.PWD;
+//let pwd = process.env.PWD;
+let pwd = process.cwd();
 let claspConfig = `${pwd}/.clasp.json`
 
 let rawData;
 try { rawData = fs.readFileSync(claspConfig); }
 catch { throw new Error(`Unable to open file: ${claspConfig}`) }
 
+console.log(`Found .clasp.json file in ${pwd}`)
+
 let json = JSON.parse(rawData);
 let scriptId = json?.scriptId;
 let fileId = json?.fileId;
 let openOnDev = json?.openOnDev;
-if (openOnDev){
+if (openOnDev) {
 	if (fileId) {
 		console.log(`Opening up Presentation: ${fileId}`)
 		open(`https://docs.google.com/presentation/d/${fileId}`);
 	}
-	else { throw new Error("File Id does not exist in .clasp.json file"); }
+	else { console.warn("File Id does not exist in .clasp.json file. Will not attempt to open File"); }
 
 	if (scriptId) {
 		console.log(`Opening up Execution Log for App Script Project: ${scriptId}`)
 		open(`https://script.google.com/home/projects/${scriptId}/executions`);
 	}
-	else { throw new Error("Script Id does not exist in .clasp.json file"); }
+	else { console.warn("Script Id does not exist in .clasp.json file.  Will not open execution log."); }
 }
+else { console.log('openOnDev set to false, will not auto open anything.') }
